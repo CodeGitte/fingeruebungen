6b642151ed8ef78d677f974790df2af7fb862504
Margit Hain <71278816+CodeGitte@users.noreply.github.com>
2022-01-17 19:58:29 +0100

    Delete 20220117_mostfrequentstring.py

--- 20220117_mostfrequentstring.py
+++
@@ -1,9 +0,0 @@
-def most_frequent(list):
-    """Takes a list of strings, returns the most frequent string within the list."""
-    return max(list, key = lambda x: list.count(x))
-
-
-if __name__ == "__main__":      
-    assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
-    assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
-
