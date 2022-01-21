e4caa1d37ad4f64c186f4c0f97f38f21e64ff50b
Margit Hain <71278816+CodeGitte@users.noreply.github.com>
2022-01-21 15:26:38 +0100

    20220121_usage of classes to write a little game

---
+++ 20220121_game.py
@@ -0,0 +1,14 @@
+# Following along a game
+# create a Pokemon
+
+
+class Pokemon:      #CamelCase
+    pass
+
+bulbasaur = Pokemon()
+bulbasaur.name = 'bulbi'
+
+
+# take a look at it
+# feed them to increase health
+# make them battle and decide for a winner
